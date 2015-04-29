af0dfcdedea0ac4535295aef2a1f66b8d1eae745
David Sena <sena.ufc@gmail.com>
2015-04-29 19:30:49 -0300

    Conta seca

--- Banco/src/banco/conta/aluno/Conta.java
+++ Banco/src/banco/conta/aluno/Conta.java
@@ -1,61 +1,38 @@
 package banco.conta.aluno;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class Conta{
-	private int numeroConta;
-	private float saldo;
-	private List<Float> extrato;
+	//defina suas variáveis de classe//TODO
 	
 	// Construtor da classe conta
 	public Conta(int numeroConta, float saldoInicial) {
-		this.numeroConta = numeroConta;
-		this.saldo = saldoInicial;
-		extrato = new ArrayList<Float>();
-		extrato.add(saldo);
+		//inicialize suas variáveis//TODO
 	}
 	
 	// Retorna o número da conta
     public int getNumeroConta() {
-		return numeroConta;
+		return 0;//TODO
 	}
 
 	// Retorna o saldo atual da conta
     public float getSaldo() {
-		return saldo;
+		return 0.0f;//TODO
 	}
 
 	// Deposita se valor não for negativo e atualiza o extrato em caso de sucesso no depósito
     public boolean depositar(float valor) {
-		if(valor >= 0) {
-			saldo += valor;
-			extrato.add(saldo);
-			return true;
-		}
-		return false;
+		return false;//TODO
 	}
 
 	// Saca se saldo suficiente e o valor não for negativo e atualiza o extrato em caso de sucesso no saque
     public boolean sacar(float value) {
-		if(value >= 0 && value <= saldo) {
-			saldo -= value;
-			extrato.add(saldo);
-			return true;
-		}
-		return false;
+		return false;//TODO
 	}
 	
 	// Saca de uma conta e deposita da conta
 	public boolean transferir(Conta conta, float valor) {
-		if(sacar(valor))
-			if(!conta.depositar(valor)) {
-				depositar(valor);
-				return false;
-			} else {
-				return true;
-			}
-		return false;
+		return false;//TODO
 	}
 
 	/* Retorna o relatório de transações da conta 
@@ -78,7 +55,7 @@ public class Conta{
      * 0.0 e 17.0 são os valores de abertura e saldo final
      */
 	public List<Float> getExtrato() {
-		return extrato;
+		return null;//TODO
 	}
 
 }
